7ccf866381a079d95271f39059d07be5bf8e045e
sadpoteto <106029162+sadpoteto@users.noreply.github.com>
2024-01-08 01:04:14 +0100

    Add buttons

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ let add = (x,y) => x+y;
 let sub = (x,y) => x-y;
 let mult = (x,y) => x*y;
 let div = (x,y) => x/y; //JS handles x/0 as NaN already
+
+let num1;
+let operator;
+let num2; 
+
+function operate(x,op,y) {
+    switch(op) {
+    case "+":
+        return add(parseInt(x), parseInt(y));
+    case "-":
+        return sub(parseInt(x), parseInt(y));
+    case "*":
+        return mult(parseInt(x), parseInt(y));
+    case "/":
+        return div(parseInt(x), parseInt(y));
+    }
+}

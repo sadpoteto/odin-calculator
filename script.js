e97d8542adc0124f4fbfdb90da2eebadbefd5e43
sadpoteto <106029162+sadpoteto@users.noreply.github.com>
2024-01-08 19:55:11 +0100

    Implement and hook operation eval / chaining
    The following now works:
      - +-*/ actually do something
      - chaining operations evaluates the previous one and saves the value
      to num1 and the display
      - = evaluates any already saved operation (i.e. clears operator)
      - clear clears everything, as per specification
      - +-Infinity yield a pedantic and snarky calculator
    (still TODO: decimal point and floats)

--- script.js
+++ script.js
@@ -3,28 +3,59 @@ let sub = (x,y) => x-y;
 let mult = (x,y) => x*y;
 let div = (x,y) => x/y; //JS handles x/0 as NaN already
 
-let num1;
-let operator;
-let num2; 
+
+const NUMDISPLAY_DEFAULT = null;
+const OPERATOR_DEFAULT = null;
+
+let num1 = null;
+let operator = OPERATOR_DEFAULT;
+let num2 = null; 
 let numDisplay = 0;
 
 let display = document.querySelector("#display-contents");
 let clrButton = document.querySelector("#clr");
 let numberButtons = document.querySelectorAll(".row > .num");
+let opButtons = document.querySelectorAll(".row > .op");
+let evalButton = document.querySelector(".row > .eval");
 
 numberButtons.forEach((button) => {
     button.addEventListener("click", (event) => {
-        numDisplay = parseInt(`${numDisplay}${event.target.getAttribute("id")}`);
+        switch(numDisplay == NUMDISPLAY_DEFAULT) {
+        case true: 
+            numDisplay = event.target.getAttribute("id");
+            break;
+        case false: 
+            numDisplay = parseInt(`${numDisplay}${event.target.getAttribute("id")}`);
+            break;
+        }
         updateDisplay();
     });
 });
 
+opButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        parseOperation();
+        operator = event.target.getAttribute("id");
+        numDisplay = NUMDISPLAY_DEFAULT
+    });
+});
+
+evalButton.addEventListener("click", (event) => {
+    parseOperation();
+    operator = OPERATOR_DEFAULT;
+});
+
 clrButton.addEventListener("click",(event) => {
-    numDisplay = 0;
+    numDisplay = NUMDISPLAY_DEFAULT;
+    num1 = null;
+    num2 = null;
+    operator = OPERATOR_DEFAULT;
     updateDisplay();
 });
 
 function operate(x,op,y) {
+    console.table(`Evaluating ${x} ${op} ${y}`);
+    if(x == null || y == null) return null;
     switch(op) {
     case "+":
         return add(parseInt(x), parseInt(y));
@@ -35,8 +66,47 @@ function operate(x,op,y) {
     case "/":
         return div(parseInt(x), parseInt(y));
     }
+    return null;
+}
+
+function parseOperation() {
+    if(operator != OPERATOR_DEFAULT) {
+        num2 = numDisplay;
+        let res = operate(num1, operator, num2);
+        if(res != null) {
+            num1 = res;
+            numDisplay = res; 
+            updateDisplay();
+        }
+    }
+    else {
+        num1 = numDisplay;
+        updateDisplay();
+    }
+}
+
+function handleInfinity() {
+    display.style.fontSize = "2.8vmin";
+    display.textContent = "I'm not that easy to break";
+    alert("You really think you can outsmart a perfect, immortal machine?");
 }
 
 function updateDisplay() {
-    display.textContent = numDisplay;
+    display.style.fontSize = "5vmin";
+    switch(numDisplay) {
+    case NUMDISPLAY_DEFAULT:
+        display.textContent = 0;
+        break;
+    case Infinity:
+        handleInfinity();
+        break;
+    case -Infinity:
+        handleInfinity();
+        break;
+    default: 
+        display.textContent = numDisplay;
+    }
+    //DEFAULT indicates no input, the user may desire inputting 0
+    //This prevents sequential operator presses from evaluating
+    //with num2 = 0 which would delete the user's calculations
 }
